92e20e8b55c9f8a13ebf77a1fa30872c9275fe5f
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-10-08 04:30:23 -0400

    Update nginx-termux.sh

--- nginx-termux.sh
+++ nginx-termux.sh
@@ -11,25 +11,15 @@ termux-wake-lock
 
 termux-setup-storage
 
-cd /data/data/com.termux/files/home/tollstream-RTMP-server/
+pkg update -y 
 
-pkg update -y && pkg install curl proot tar -y && curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu/ubuntu.sh | bash
-
-pkg upgrade
-
-apt-get install man git wget -y
-
-echo 'command+=" -b /data/data/com.termux/files/home:/root"' | tee -a start-ubuntu.sh
-
-echo 'command+=" -b /sdcard"' | tee -a  start-ubuntu.sh
+pkg upgrade -y
 
 apt-get autoremove
 
-chmod +wrx nginx-ubuntu-rtmp2.sh
+pkg install proot-distro
 
-cp nginx-ubuntu-rtmp2.sh /data/data/com.termux/files/home/tollstream-RTMP-server/ubuntu-fs/home
-
-./start-ubuntu.sh
+proot-distro install ubuntu
 
 exit
 
