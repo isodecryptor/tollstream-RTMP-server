e78362377b43ca4b7414555388a4ba028fd97292
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-10-09 14:59:22 -0400

    Update nginx-ubuntu-rtmp2.sh

--- nginx-ubuntu-rtmp2.sh
+++ nginx-ubuntu-rtmp2.sh
@@ -6,44 +6,29 @@
 
 #PURPOSE: To install an nginx rtmp server 
 #for use with tollstream.com
+
 apt-get update 
 
 apt-get upgrade
 
 apt-get install nginx
 
-apt-get install libnginx-mod-rtmp ufw
+apt-get install libnginx-mod-rtmp ufw git
 
 apt-get install sudo
 
 sudo apt-get autoremove
 
-echo "Please choose a user name to use with sudo"
-echo "Root user will be removed for security reasons"
+cd $home
 
-# Purpose - Script to add a user to Linux system including passsword
-# Author - Vivek Gite  under GPL v2.0+
-# ------------------------------------------------------------------
-# Am i Root user?
-if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username : " username
-	read -s -p "Enter password : " password
-	egrep "^$username" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "$username exists!"
-		exit 1
-	else
-		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-		useradd -m -p "$pass" "$username"
-		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-		usermod -aG sudo $username
-                echo "$username has been added to sudoers file"
-                echo "$username now has sudo priveleges"
-	fi
-else
-	echo "Only root may add a user to the system."
-	exit 2
-fi
+git clone https://github.com/isodecryptor/tollstream-RTMP-server
+
+sudo rm /etc/nginx
+
+mv /root/tollstream-RTMP-server/nginx.conf /etc/nginx/nginx.conf
+
+
+
 exit
 
 
