69e6d27ea1a97a164e7ef5a5dda265d32b3d8de5
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-10-09 22:44:31 -0400

    Update nginx-ubuntu-rtmp.sh

--- nginx-ubuntu-rtmp.sh
+++ nginx-ubuntu-rtmp.sh
@@ -1,24 +1,14 @@
-#$BASH
-
 #Author:Donald Bilton
 #
 #Date: 10/07/2020
-
 #PURPOSE: To install an nginx rtmp server 
 #for use with tollstream.com
-
 apt-get update 
-
 apt-get upgrade
-
 apt-get install libnginx-mod-rtmp ufw git nginx
-
 sudo apt-get autoremove
-
 sudo rm /etc/nginx/nginx.conf
-
 mv /root/tollstream-RTMP-server/nginx.conf /etc/nginx/nginx.conf
-
 exit
 
 
