970ab9ae3a1f4aee4037272221c824d89c525a4f
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-10-13 17:51:13 -0400

    Create tollstream-RTMP-ubuntu4.sh

---
+++ tollstream-RTMP-ubuntu4.sh
@@ -0,0 +1,31 @@
+#Author:Donald Bilton
+#
+#Date: 10/07/2020
+#PURPOSE: To install an nginx rtmp server
+#for use with tollstream.com
+apt-get update -y
+apt-get upgrade -y
+apt-get install libnginx-mod-rtmp nginx
+apt-get autoremove -y
+rm /etc/nginx/nginx.conf
+mv /$home/tollstream-RTMP-server/nginx.conf /etc/nginx/nginx.conf
+echo nginx -t
+echo "An RTMP server requires that you have port 1935 open. This includes all hardware and software, firwealls \n"
+echo "If you are behind nat firewall and dont have access to all the network administration,\n"
+echo "please answer no and a nat bypass service will be installed."
+echo "This will allow port 1935 to be reached from behind \n"
+echo "Will you be port forwarding tcp 1935 or need /n"
+echo "the local tunneling solution, provided by this script?"
+echo "Please enter a y for yes or an n for no"
+read answ1                                            echo $answ1
+while [[ "$answ1" != [yYnN] ]]; do
+   echo "Please enter a y for yes or an n for no"
+   read answ1
+done
+if [ "$answ1" = "y" ] || [ "$answ1" = "Y" ]; then
+   apt install npm
+   npm install -g localtunnel
+else
+   echo "Skipping local tunnel nat bypass install"
+fi
+exit
