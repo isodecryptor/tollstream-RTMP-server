9b6cc2c22941e94c121a54fb3998bdee420a2173
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-10-13 23:56:21 -0400

    Update tollstream-RTMP-ubuntu3.sh

--- tollstream-RTMP-ubuntu3.sh
+++ tollstream-RTMP-ubuntu3.sh
@@ -7,7 +7,7 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install libnginx-mod-rtmp nginx
 sudo apt-get autoremove -y
-rm /etc/nginx/nginx.conf
+sudo rm /etc/nginx/nginx.conf
 mv /home/tollstream-RTMP-server/nginx.conf /etc/nginx/nginx.conf
 echo nginx -t
 echo "An RTMP server requires that you have port 1935 open. This includes all hardware and software, firwealls \n"
