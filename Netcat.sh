a60eabe459acce4b6c1fba9db7341c4c5e470a98
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-10-15 23:28:28 -0400

    Create Netcat.sh

---
+++ Netcat.sh
@@ -0,0 +1,13 @@
+#basic netcat usage to report server details to eccomerce
+Server..
+
+
+#nc 52.86.45.108 2001 < netcat.test
+#file transfer for client...
+
+#for sercver tollatream
+
+#  sudo nc 2001 >> netcat.txt
+
+#this appends clients to end of netcat>
+#in tollstream.
