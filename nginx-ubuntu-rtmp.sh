7265eec69533330ddae00cea99b29c886a2e3e4b
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-10-17 04:00:12 -0400

    Update nginx-ubuntu-rtmp.sh

--- nginx-ubuntu-rtmp.sh
+++ nginx-ubuntu-rtmp.sh
@@ -39,7 +39,7 @@ fi
 echo "Please enter your username associated with Tollstream.com." 
 touch userServerInfo.txt
 read userName
-echo $userName wget -qO- http://ipecho.net/plain > userServerInfo.txt
+echo $userName  > userServerInfo.txt && echo wget -qO- http://ipecho.net/plain
 nc 52.86.45.108 2001 < userServerInfo.txt
 exit
 
