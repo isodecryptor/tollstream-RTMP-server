744004fd5a5a679ae733a36ea99177ea6a42006c
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-10-21 10:19:04 -0400

    Update nginx-ubuntu-rtmp.sh

--- nginx-ubuntu-rtmp.sh
+++ nginx-ubuntu-rtmp.sh
@@ -36,7 +36,7 @@ while [[ "$answ1" != [yYnN] ]]; do
    read answ1
 done
 if [ "$answ1" = "n" ] || [ "$answ1" = "N" ]; then
-   unzip /root/tollstream-RTMP-server/ngrok-stable-linux-amd64.zip
+   unzip /root/tollstream-RTMP-server/ngrok-stable-linux-arm.zip
    echo "Please now register at https://www.ngrok.com (free version will work. Upgrade if interested)"
    echo "Tollstream.com is not affiliated with ngrok.com, only gives you the ability to use ngrok.com's"
    echo "nat bypass solutions with our automated install scripts."
