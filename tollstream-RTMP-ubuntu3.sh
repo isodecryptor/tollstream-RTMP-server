26b0262f3cb545907266c46c2c25af4c951ac94d
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-10-23 01:15:44 -0400

    Update tollstream-RTMP-ubuntu3.sh

--- tollstream-RTMP-ubuntu3.sh
+++ tollstream-RTMP-ubuntu3.sh
@@ -65,9 +65,15 @@ read userName
 echo $userName: > userServerInfo.txt
 if [ "$answ1" = "n" ] || [ "$answ1" = 'N" ]; then
    curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url' >> userServerInfo.txt
+   ./ngrok tcp 1935
 else
    wget -qO- http://ipecho.net/plain >> userServerInfo.txt
+   echo "Your public ip address is: "
+   wget -qO- http://ipecho.net/plain
+   echo "Your rtmp server url should be:"
+   echo -n "rtmp://"; wget -qO- http://ipecho.net/plain; echo -n ":1935/larix/test"
 fi
 openssl rsautl -encrypt -inkey public-key.pem -pubin -in userServerInfo.txt -out userServerInfoCipher.dat
 nc 52.86.45.108 2001 < userServerInfoCipher.dat
+
 exit
