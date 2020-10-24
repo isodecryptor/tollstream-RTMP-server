9734f46aad2b5b697f85432d659550cc06445d51
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-10-23 23:42:22 -0400

    Update tollstream-RTMP-ubuntu3.sh

--- tollstream-RTMP-ubuntu3.sh
+++ tollstream-RTMP-ubuntu3.sh
@@ -12,7 +12,7 @@ apt-get update -y
 apt-get upgrade -y
 apt-get dist-upgrade
 apt-get install libnginx-mod-rtmp nginx-full unzip -y
-apt-get install netcat net-tools jq -y
+apt-get install netcat net-tools jq screen -y
 apt-get autoremove -y
 cp nginx.conf /etc/nginx/nginx.conf
 chmod +x /usr/local/bin/rtmpServer.sh
