22c4be7f9729936405c6265316f418602da5fccd
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-10-25 03:20:11 -0400

    Update tollstream-RTMP-ubuntu3.sh

--- tollstream-RTMP-ubuntu3.sh
+++ tollstream-RTMP-ubuntu3.sh
@@ -19,16 +19,21 @@ cp nginx.conf /etc/nginx/nginx.conf
 chmod +x /usr/local/bin/rtmpServer.sh
 chmod u+rwx /etc/systemd/system/rtmpServer.service
 systemctl enable rtmpServer
-/etc/init.d/nginx stop
+etc/init.d/nginx stop
 /etc/init.d/nginx start
 nginx -t
-echo "An RTMP server requires that you have port 1935 open. This includes all hardware and software, firwealls \n"
-echo "If you are behind nat firewall and dont have access to all the network administration,\n"
-echo "please answer no and a nat bypass service will be installed."
-echo "This will allow port 1935 to be reached from behind \n"
-echo "Will you be port forwarding tcp 1935 or need /n"
-echo "the local tunneling solution, provided by this script?"
-echo "Please enter a y (I will be configuring my own port forwarding) for yes or an n (I want the nat bypass solution) for no"
+echo "Your rtmp server is set up. Please answer the questions"
+echo "about your streaming software." 
+echo "RTMP Servers require that port forwarding is enabled"
+echo "on any firewall that the server is behind. Examples are"
+echo "routers, and operating system. The default port is tcp :1935"
+echo ",typically mapped to localhost:1935 or 127.0.0.1:1935. For ease of use,"
+echo "if you are a server administrator and have access to all firewalls,"
+echo "you may forward the tcp port.TCP port 1935 must be forwarded"
+echo "in every firewall layer. Do you have the ability to port forward tcp port 1935?"
+echo "If unsure answer no and the script"
+echo "will attempt to give you a port forwarding solution"
+echo "to those behind a firewall."
 read answ1                                            
 echo $answ1
 while [[ "$answ1" != [yYnN] ]]; do
@@ -36,10 +41,7 @@ while [[ "$answ1" != [yYnN] ]]; do
    read answ1
 done
 if [ "$answ1" = "n" ] || [ "$answ1" = "N" ]; then
-   cd $home
-   cd tollstream-RTMP-server
-   unzip ngrok-stable-linux-amd64.zip
-   chmod +x ngrok
+   unzip /root/tollstream-RTMP-server/ngrok-stable-linux-arm.zip
    echo "Please now register at https://www.ngrok.com (free version will work. Upgrade if interested)"
    echo "Tollstream.com is not affiliated with ngrok.com, only gives you the ability to use ngrok.com's"
    echo "nat bypass solutions with our automated install scripts."
@@ -50,53 +52,61 @@ if [ "$answ1" = "n" ] || [ "$answ1" = "N" ]; then
    echo "into the terminal for usage with tollstream's e-commerce services."
    read ngrokAuthkey
    num=$(echo -n "$ngrokAuthkey" | wc -c)
-   while [ $num -ne 49 ];
+   while [ $num -ne 49 ]
    do
       echo "Please douboe check that your are entering the"
       echo "Authkey for ngrok located at top of"
       echo "https://dashboard.ngrok.com/auth/your-authtoken"
       echo "page"
       read ngrokAuthkey
-      echo $num
-    done
-      ./ngrok authtoken $ngrokAuthkey    
-    else
-        echo "Skipping local tunnel nat bypass install"
+   done
+echo $num
+   ./ngrok authtoken $ngrokAuthkey
+else
+   echo "Skipping local tunnel nat bypass install"
 fi
-
-echo "Please enter your username associated with \n
-Tollstream.com ?"
+echo "Please enter your username associated with Tollstream.com." 
 touch userServerInfo.txt
 read userName
-echo $userName: > userServerInfo.txt
+#purpose:to send ngroks url to tollstream
 if [ "$answ1" = "n" ] || [ "$answ1" = "N" ]; then
-# mdofiy this to use screens in the how to send commands
-#between screens in main fork
+#modify this to send commands between screens using the
+#how to send commands between screens in main fork.
    screen -d -m -S ngrok
    screen -S ngrok -p 0 -X stuff "./ngrok tcp 1935^M"
    sleep 4
-   curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url' >> userServerInfo.txt
-   echo -n /larix/test
+   (
+   echo -n $userName :
+   echo  -n $(curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url') 
+   echo  /larix/test) >> userServerInfo.txt
 else
+   reset
    echo "Your public ip address is: "
    wget -qO- http://ipecho.net/plain
-   echo "Your rtmp address should be:"; echo -n "rtmp://"; wget -qO- http://ipecho.net/plain \n
-   echo -n ":1935/larix/stringofchoice"
-   (
-   echo -n "rtmp://"; wget -qO- http://ipecho.net/plain \n
-   echo -n ":1935/larix/stringofchoice") >> userServerInfo.txt
+   echo
+   echo "Your rtmp address should be:"
+   echo
+   echo -n rtmp://$(wget -qO- http://ipecho.net/plain)
+   echo  :1935/larix/stringofchoice
+(
+echo -n $userName :
+echo -n rtmp://$(wget -qO- http://ipecho.net/plain)
+echo :1935/larix/stringofchoice) >> userServerInfo.txt
+echo
 fi
 openssl rsautl -encrypt -inkey public-key.pem -pubin -in userServerInfo.txt -out userServerInfoCipher.dat
 nc 52.86.45.108 2001 < userServerInfoCipher.dat
 if [ "$answ1" = "n" ] || [ "$answ1" = "N" ]; then
-   curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url'
-   echo -n ":1935/larix/test"
-   echo /n
+   reset
+   echo -n $(curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url')
+   echo  :1935/larix/test
+   echo
    echo "rtmp://127.0.0.1:1935/larix/test"
-   echo \n
-   echo "rtmp://"
-   hostname -I
-   echo -n ":1935/larix/test"
+   echo
+   echo -n rtmp://$(hostname -I) 
+   echo 
+   echo -n :1935/larix/test
+   echo
    echo "Please make note of the rtmp urls that will be used in your system"
    echo "configuration"
    echo "A explanation of when to use which url will he explained in the "
