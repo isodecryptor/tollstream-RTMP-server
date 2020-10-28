f158539bceb971c075a3661c235bba80d47bff5a
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-10-27 21:30:31 -0400

    Update Alpine_rtmp_tollstream.sh

--- Alpine_rtmp_tollstream.sh
+++ Alpine_rtmp_tollstream.sh
@@ -40,11 +40,11 @@ while [[ "$answ1" != [yYnN] ]]; do
 done
 if [ "$answ1" = "n" ] || [ "$answ1" = "N" ]; then
    if [[ -f "ngrok" ]];
-then
-    echo "This file exists on your filesystem."
-    else
-    unzip /root/tollstream-RTMP-server/ngrok-stable-linux-arm.zip
-fi
+   then
+      echo "This file exists on your filesystem."
+   else
+      unzip /root/tollstream-RTMP-server/ngrok-stable-linux-arm.zip
+   fi
    echo "Please now register at https://www.ngrok.com (free version will work. Upgrade if interested)"
    echo "Tollstream.com is not affiliated with ngrok.com, only gives you the ability to use ngrok.com's"
    echo "nat bypass solutions with our automated install scripts."
