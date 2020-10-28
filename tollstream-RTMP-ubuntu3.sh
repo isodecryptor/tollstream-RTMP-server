c84367258d987270e2d126b4e5016fe50c0b0c88
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-10-28 00:55:27 -0400

    Update tollstream-RTMP-ubuntu3.sh

--- tollstream-RTMP-ubuntu3.sh
+++ tollstream-RTMP-ubuntu3.sh
@@ -64,7 +64,7 @@ fi
    echo "into the terminal for usage with tollstream's e-commerce services."
    read ngrokAuthkey
    num=$(echo -n "$ngrokAuthkey" | wc -c)
-   while [[ $num -gt 50 ]] || [[ $num -lt 45 ]]
+   while [ $num -gt 50 ] || [ $num -lt 45 ];
    do
       echo "Please douboe check that your are entering the"
       echo "Authkey for ngrok located at top of"
