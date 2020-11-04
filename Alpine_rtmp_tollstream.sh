3c52e50ed2e35c040a64e5d435665f49bc858c88
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-11-04 17:14:11 -0500

    Update Alpine_rtmp_tollstream.sh

--- Alpine_rtmp_tollstream.sh
+++ Alpine_rtmp_tollstream.sh
@@ -36,17 +36,17 @@ else
     touch /data/data/com.termux/files/home/prestart.sh
     chmod +x /data/data/com.termux/files/home/prestart.sh
     ( #echo "#!/bin/bash"
-   # echo "screen -d -m -S startup"
-  #  echo 'screen -S startup -p 0 -X stuff "proot-distro login alpine^M"' 
-  #  echo 'screen -S startup -p 0 -X stuff "cd /home/tollstream-RTMP-server^M"' 
-  #  echo 'screen -S startup -p 0 -X stuff "./Tollstreamstartup.sh^M"' 
-   # echo "screen -r startup"
-   # echo "exit"  
+    echo "screen -d -m -S startup"
+    echo 'screen -S startup -p 0 -X stuff "proot-distro login alpine^M"' 
+    echo 'screen -S startup -p 0 -X stuff "cd /home/tollstream-RTMP-server^M"' 
+    echo 'screen -S startup -p 0 -X stuff "./Tollstreamstartup.sh^M"' 
+    echo "screen -r startup"
+    echo "exit"  
     ) > /data/data/com.termux/files/home/prestart.sh
     ( 
     
     echo "/data/data/com.termux/files/home/prestart.sh"
-    ) >> /data/data/com.termux/files/usr/etc/bash.bashrc
+    ) >> /data/data/com.termux/files/usr/etc/profile
 fi
 #define variables here
 
