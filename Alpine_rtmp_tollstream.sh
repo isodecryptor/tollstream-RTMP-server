68e0385db90fb50667c8a2ef5b0fed1282a6fd67
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-11-04 00:04:08 -0500

    Update Alpine_rtmp_tollstream.sh

--- Alpine_rtmp_tollstream.sh
+++ Alpine_rtmp_tollstream.sh
@@ -25,7 +25,7 @@ else
     echo   "cd /home/tollstream-RTMP-server"
     echo   "./Alpine_rtmp_tollstream.sh"\
     echo   "fi" ) > Tollstreamstartup.sh
-    ( echo "screen -d -m -S startup"
+    ( #echo "screen -d -m -S startup"
     echo 'screen -S startup -p 0 -X stuff "proot-distro login alpine^M"'
     echo 'screen -S startup -p 0 -X stuff "cd /home/tollstream-RTMP-server^M"'
     echo 'screen -S startup -p 0 -X stuff "./Tollstreamstartup^M"'
