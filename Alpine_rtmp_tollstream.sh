38bdfc16c5c6c870ed7a8828cbccf6cbadfc2626
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-11-04 10:22:06 -0500

    Update Alpine_rtmp_tollstream.sh

--- Alpine_rtmp_tollstream.sh
+++ Alpine_rtmp_tollstream.sh
@@ -32,18 +32,18 @@ else
     echo   "fi" ) > /home/tollstream-RTMP-server/Tollstreamstartup.sh
     #Find the correct directory for prestart so no recursive loop starts inside bash.bashrc. Must call external bash shell script in bash.bashrc, otherwise,
     #infinite recursive loop will occur because of it continously calling bash or the author of termux is being a, secretive , douche and hiding some game. 
-    touch /data/data/com.termux/files/usr/prestart.sh"
-    chmod +x /data/data/com.termux/files/usr/prestart.sh"
+    touch /data/data/com.termux/files/home/prestart.sh"
+    chmod +x /data/data/com.termux/files/home/prestart.sh"
     ( echo "#!/bin/bash"
     echo "screen -d -m -S startup"
     echo 'screen -S startup -p 0 -X stuff "proot-distro login alpine^M"' 
     echo 'screen -S startup -p 0 -X stuff "cd /home/tollstream-RTMP-server^M"' 
     echo 'screen -S startup -p 0 -X stuff "./Tollstreamstartup^M"' 
     echo "screen -r startup"   
-    ) > /data/data/com.termux/files/usr/prestart.sh
+    ) > /data/data/com.termux/files/home/prestart.sh
     ( 
     
-    echo "data/data/com.termux/files/usr/prestart.sh"
+    echo "data/data/com.termux/files/home/prestart.sh"
     ) >> /data/data/com.termux/files/usr/etc/bash.bashrc
 fi
 #define variables here
