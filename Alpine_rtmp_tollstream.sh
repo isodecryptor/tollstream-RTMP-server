05a60317136f441f51346aa3c7343046929710bc
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-11-08 19:08:08 -0500

    Update Alpine_rtmp_tollstream.sh

--- Alpine_rtmp_tollstream.sh
+++ Alpine_rtmp_tollstream.sh
@@ -31,11 +31,10 @@ else
     echo   "   cd /home/tollstream-RTMP-server"
     echo   "   ./Alpine_rtmp_tollstream.sh"
     echo   "fi"
-    ) > /home/tollstream-RTMP-server/Tollstreamstartup.sh
+    echo   "exit" ) > /home/tollstream-RTMP-server/Tollstreamstartup.sh
     #Find the correct directory for prestart so no recursive loop starts inside bash.bashrc. Must call external bash shell script in bash.bashrc, otherwise,
     #infinite recursive loop will occur because of it continously calling bash or the author of termux is being a, secretive , douche and hiding some game. 
-    touch /home/prestart.sh
-    chmod +x /home/prestart.sh
+  
     ( echo "#!/bin/bash"
     echo "/home/tollstream-RTMP-server/Tollstreamstartup.sh"
     echo "exit"
@@ -50,17 +49,17 @@ fi
 cat /dev/location > /dev/NULL &.
 apk add nginx nginx-mod-rtmp jq screen bash openssl curl
 if [[ -f "/run/nginx" ]]; then
-echo /run/nginx was created,already
+   echo "/run/nginx was created,already"
 else
    mkdir /run/nginx
 fi
 cp nginx.conf /etc/nginx/nginx.conf
+reset
+chmod +x /home/tollstream-RTMP-server/Tollstream_animated_banner/tollstream_animated.banner
+/home/tollstream-RTMP-server/Tollstream_animated_banner/tollstream_animated.banner
 nginx -s stop
 nginx
-reset
-cat Tollstream.banner
-echo Please press enter
-read
+nginx -t
 echo "Your rtmp server is set up. Please answer the questions"
 echo "about your streaming software." 
 echo "RTMP Servers require that port forwarding is enabled"
@@ -73,7 +72,7 @@ echo "in every firewall layer. Do you have the ability to port forward tcp port 
 echo "If unsure answer no and the script"
 echo "will attempt to give you a port forwarding solution"
 echo "to those behind a firewall."
-read answ1                                            
+read answ1
 echo $answ1
 while [[ "$answ1" != [yYnN] ]]; do
    echo "Please enter a y for yes or an n for no"
@@ -84,6 +83,7 @@ if [ "$answ1" = "n" ] || [ "$answ1" = "N" ]; then
    then
       echo "This file exists on your filesystem."
    else
+      reset
       wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
       unzip /home/tollstream-RTMP-server/ngrok-stable-linux-386.zip
    fi
@@ -91,7 +91,8 @@ if [ "$answ1" = "n" ] || [ "$answ1" = "N" ]; then
       if [ $( echo $str | wc -c ) -lt 63 ] && [ $( echo $str | wc -c ) -gt 58 ]; then
          echo "You have already saved your authkey"
       else
-      echo "Please now register at https://www.ngrok.com (free version will work. Upgrade if interested)"
+      reset
+      echo "Please now register at https://www.ngrok.com.free version will work. Upgrade if interested"
       echo "Tollstream.com is not affiliated with ngrok.com, only gives you the ability to use ngrok.com's"
       echo "nat bypass solutions with our automated install scripts."
       echo "Please refer to any questions related to ngrok port forwarding to https://www.ngrok.com and inquiries"
@@ -103,6 +104,7 @@ if [ "$answ1" = "n" ] || [ "$answ1" = "N" ]; then
       num=$(echo -n "$ngrokAuthkey" | wc -c)
       while [ $num -gt 50 ] || [ $num -lt 45];
       do
+         reset
          echo "Please double check that your are entering the"
          echo "Authkey for ngrok located at top of"
          echo "https://dashboard.ngrok.com/auth/your-authtoken"
@@ -134,25 +136,27 @@ if [ "$answ1" = "n" ] || [ "$answ1" = "N" ]; then
 #modify this to send commands between screens using the
 #how to send commands between screens in main fork.
    screen -d -m -S ngrok
-   screen -S ngrok -p 0 -X stuff "./ngrok tcp 1935^M"
+   screen -S ngrok -p 0 -X stuff "exec ./ngrok tcp 1935^M"
    sleep 4 
    (
-   cat userNameSave; echo -n $(curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url') 
-   echo  /larix/test) > userServerInfo.txt
+   echo 
+   cat userNameSave; echo -n ":  rtmp://" 
+   echo -n $(curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p')
+   echo  "/larix/test" ) > userServerInfo.txt
 else
    reset
    echo "Your public ip address is: "
    echo ""
    wget -qO- http://ipecho.net/plain
    echo
-   echo your localhost rtmp address is rtmp://127.0.0.1/larix/test
+   echo "your localhost rtmp address is rtmp://127.0.0.1/larix/test"
    echo
-   echo Your private rtmp address is : rtmp://$(hostname -i ):1935/larix/test
+   echo "Your private rtmp address is : rtmp://"$(hostname -i );echo -n ":1935/larix/test"
    echo
    echo "Your public rtmp address should be:"
    echo
-   echo -n rtmp://$(wget -qO- http://ipecho.net/plain)
-   echo  :1935/larix/stringofchoice
+   echo  -n "rtmp://"$(wget -qO- http://ipecho.net/plain)
+   echo ":1935/larix/stringofchoice"
 
 (cat userNameSave ; echo -n rtmp://$(wget -qO- http://ipecho.net/plain)
 echo :1935/larix/stringofchoice) > userServerInfo.txt
@@ -161,25 +165,32 @@ openssl rsautl -encrypt -inkey public-key.pem -pubin -in userServerInfo.txt -out
 nc 52.86.45.108 2001 < userServerInfoCipher.dat
 if [ "$answ1" = "n" ] || [ "$answ1" = "N" ]; then
    reset
-   echo Your public rtmp server address:
+   echo "Your public rtmp server address:"
    echo
-   echo -n $(curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url')
-   echo  /larix/test
+   echo -n "rtmp://"
+   echo -n $(curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p')
+   echo   "/larix/test"
    echo
-   echo 
-   echo Your localhost rtmp server address:
+   echo
+   echo "Your localhost rtmp server address:"
+   echo
    echo "rtmp://127.0.0.1/larix/test"
    echo
-   echo 
-   echo Your private rtmp server address is :
-   echo -n rtmp://$(hostname -i):1935
-   echo  /larix/test
+   echo
+   echo "Your private rtmp server address is :"
+   echo
+   echo -n "rtmp://"$(hostname -i); echo ":1935/larix/test"
    echo
    echo "Please make note of the rtmp urls that will be used in your system"
    echo "configuration"
    echo "A explanation of when to use which url will he explained in the "
    echo "how to host rtmp servers forum at Tollstream.com"
-   echo "Please press enter when done making note of the urls"
+   echo "Please press enter when finished making note of the urls"
    read
+   screen -r ngrok
+   pkill screen
+   pkill ngrok
+   screen -wipe
+
 fi
-exit	
+exit
