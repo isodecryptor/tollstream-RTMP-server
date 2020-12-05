be21263145c22a44403a82ad99d74f81f09d4eb5
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-12-05 01:16:00 -0500

    Update Alpine_rtmp_tollstream.sh

--- Alpine_rtmp_tollstream.sh
+++ Alpine_rtmp_tollstream.sh
@@ -20,7 +20,7 @@ else
    ( echo "#!/bin/bash"
     echo  'if [ -d "/home/tollstream-RTMP-server" ]; then' 
     echo   "   cd /home"
-    echo   "   git pull; git stash; git pull "
+    echo   "  git config core.fileMode false; git pull; git stash; git pull "
     echo   "   cd /home/tollstream-RTMP-server"
     echo   "   ./Alpine_rtmp_tollstream.sh"
     echo   "else"
