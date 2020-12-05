00ec42c4d4d9a4b1b3a4c543ecf7692085a55793
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-12-04 20:20:37 -0500

    Update Alpine_rtmp_tollstream.sh

--- Alpine_rtmp_tollstream.sh
+++ Alpine_rtmp_tollstream.sh
@@ -20,8 +20,7 @@ else
    ( echo "#!/bin/bash"
     echo  'if [ -d "/home/tollstream-RTMP-server" ]; then' 
     echo   "   cd /home"
-    echo   "   git pull origin  Iphone" \
-           "https://github.com/isodecryptor/tollstream-RTMP-server"
+    echo   "   git pull; git stash; git pull "
     echo   "   cd /home/tollstream-RTMP-server"
     echo   "   ./Alpine_rtmp_tollstream.sh"
     echo   "else"
