baa2ba622b230fe40454ab2f95b509873731fa86
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2020-12-07 23:23:42 -0500

    Update Alpine_rtmp_tollstream.sh

--- Alpine_rtmp_tollstream.sh
+++ Alpine_rtmp_tollstream.sh
@@ -23,9 +23,25 @@ else
    chmod +x /home/tollstream-RTMP-server/Tollstreamstartup.sh
    ( echo "#!/bin/bash"
     echo   "   cd /home/tollstream-RTMP-server"
-    echo   "   git config --global core.fileMode false; git stash; git pull"
-    echo   "   cd /home/tollstream-RTMP-server"
-    echo   "   ./Alpine_rtmp_tollstream.sh"
+    echo   "   reset"
+    echo   "   echo ' Would you to check for updates for tollstream.com's server scripts and auto install?. Warning. If modifying ' "
+    echo   "   echo ' files like nginx.conf or Alpine_rtmp_tollstream.sh, manually, you ' "
+    echo   "   echo ' are advised to back up these custom configurations, as you will likely loose them. ' "
+    echo   "   echo ' Manual remerging of personal configurations are required after updating, if ' "
+    echo   "   echo ' these files have been, manually, modified. ' "
+    echo   "   echo ' Otherwise, updating is recommended and new features will be listed as they are added. ' "
+    echo   "   echo ' Please enter y for yes, update, or n for no, skip update check. ' "
+    echo   "   read update "
+    echo   "   while [[ "update" != [yYnN] ]]; do
+    echo   "   echo ' Please answer y for yes or n for no. ' "
+    echo   "   read update "
+    echo   "   done "
+    echo   "   if [ "update" = [yY] ]
+    echo   "      git config --global core.fileMode false; git stash; git pull"
+    echo   "   else"
+    echo   "      cd /home/tollstream-RTMP-server"
+    echo   "      ./Alpine_rtmp_tollstream.sh"
+    echo   "   fi "
     echo   "exit" ) > /home/tollstream-RTMP-server/Tollstreamstartup.sh
     #Find the correct directory for prestart so no recursive loop starts inside bash.bashrc. Must call external bash shell script in bash.bashrc, otherwise,
     #infinite recursive loop will occur because of it continously calling bash or the author of termux is being a, secretive , douche and hiding some game. 
