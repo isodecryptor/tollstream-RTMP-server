ec82c274cf88d8beafe54f96e1ff23d59842cebc
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2021-11-23 16:40:12 -0500

    Add files via upload

--- tollstream-alpine-chroot2.sh
+++ tollstream-alpine-chroot2.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# Attempt to install an alpine chroot for a central kernel for tollstream.com. 
+# working Attempt to install an alpine chroot for a central kernel for tollstream.com. 
 # The objective is to make a script that works with most if not all linux distributions
 # This should work with any os that has a bash shell and builtins such as chroot. 
 
+RED="\e[31m"
+LIGHTBLUE="\e[96m"
+PURPLE="\e[95m"
+ENDCOLOR="\e[0m"
+
+ENDCOLOR="\e[0m"
 
 virtual_machine() {
 
@@ -14,11 +20,15 @@ wget https://dl-cdn.alpinelinux.org/alpine/v3.14/releases/x86_64/alpine-virt-3.1
 
 tar -xvf alpine-minirootfs-*.tar
 
-echo 'Please enter the memory you would like to dedicate to the streaming server \n'
+clear
 
-echo 'Gernerally, it is safe to allocate half your memory to a multi purpose computer such as a desktop or other multi task computer'
+echo 'Please enter the memory you would like to dedicate to the streaming server'
 
-echo 'Your currently free and used memory in MB is \n'
+echo 'Gernerally, it is safe to allocate half your memory to a multi purpose computer'
+
+echo 'such as a desktop or other multi task computer'
+
+echo 'Your currently free and used memory in MB is'
 
 free -m
 
@@ -30,6 +40,7 @@ qemu-img create -f qcow2 alpine.qcow2 1G
 
 qemu-system-x86_64 -m $memory_allocate -nic user -boot d -cdrom alpine-virt-3.14.2-x86_64.iso
 
+clear
 
 }
 
@@ -44,13 +55,13 @@ tollstream_chroot() {
 
   architecture=$(uname -m)
 
-  if [ $architecture == 'unknown' ]; then 
+  if [ $architecture == 'unknown' ]; then
 
      architecture=$(arch)
 
   fi
 
-  if [ $(echo $architecture | grep armv7) == '' ]; then
+  if [ $(echo $architecture | grep armv7) =='' ]; then
 
      architecture=$architecture
 
@@ -59,7 +70,7 @@ tollstream_chroot() {
   else
 
      architecture='armv7'
-     
+
      echo $architecture
 
   fi
@@ -90,11 +101,11 @@ tollstream_chroot() {
 
   mount -v --rbind /sys Tollstream/sys
 
-  mount --make-rprivate /sys
+  mount --make-rprivate Tollstream/sys
 
   mount -v --rbind /dev Tollstream/dev
 
-  mount --make-rprivate /dev
+  mount --make-rprivate Tollstream/dev
 
   if [ -L /dev/shm ] && [ -d /run/shm ]; then
 
@@ -106,7 +117,7 @@ tollstream_chroot() {
   fi
 
   cp -L /etc/resolv.conf Tollstream/etc/
-  
+
   mkdir -p Tollstream/etc/apk
 
   echo "https://mirror.clarkson.edu/alpine/v3.14/main" > Tollstream/etc/apk/repositories
@@ -114,6 +125,39 @@ tollstream_chroot() {
   chroot Tollstream rm /etc/mtab 2> /dev/null
 
   chroot Tollstream ln -s /proc/mounts /etc/mtab
+
+#  cd /home/Tollstream
+
+
+
+
+echo $(printf "if [[ ! -f '/home/Tollstream/exist' ]];\n"
+
+printf "then\n"
+
+printf "touch /home/Tollstream/exist;\n"
+
+printf "apk update; apk upgrade;\n"
+
+printf "apk add git bash openssl screen;\n"
+
+printf "cd /home;\n"
+
+printf "git clone --depth=1 -b Iphone \\n"
+
+printf " --single-branch \\n"
+
+printf "https://github.com/isodecryptor/tollstream-RTMP-server.git;\n"
+
+printf "cd tollstream-RTMP-server;\n"
+
+printf "chmod +x Alpine_rtmp_tollstream.sh;\n"
+
+printf "./Alpine_rtmp_tollstream.sh;\n"
+
+printf "fi\n") >> /home/Tollstream/etc/profile
+
+
 
   chroot Tollstream /bin/ash -l
 
@@ -125,35 +169,49 @@ tollstream_chroot() {
 
 }
 
-echo 'Tollstream has two options for installing its open source server \n'
+clear
 
-echo 'Virtual machines are sometimes the only option you may have. It is also a more private option.Ex. your computers mac address will not be the same\n '
+echo -e "${LIGHTBLUE}Tollstream has two options for installing its open source server"
 
-echo "as the virtual machine's\n"
+echo 'Virtual machines are sometimes the only option you may have.'
 
-echo "A virtual machine will install tollstream's RTMP server in a totoally isolated environment. Nothing that is installed on the virual machine effects\n"
+echo  'It is also a more private option.Ex. your computers mac address'
 
-echo "the installation of the parent os\n"
+echo  "will not be the same as the virtual machine's"
 
-echo "A virtual machine is a great option for more privacy and security, however it does take more system resources to run than a chroot install\n"
+echo  "A virtual machine will install Tollstream's RTMP server in a totoally isolated environment."
 
-echo " The other option is a chroot install of tollstream's packages. It is isolated from your parent os as well but does use the same drivers as your parent os\n"
+echo  "Nothing that is installed on the virual machine effects" 
 
-echo "If you are running linux and not concerned with the greater privacy benefits of using a virtual machine, the chroot install option is better on system resources\n"
+echo "the installation of the parent os"
 
-echo "Tollstream uses the tiny Alpine linux distribution to be cross compatible with all operating systems(tollstream runs in a tiny, isolated os)\n"
+echo "A virtual machine is a great option for more privacy and security,"
 
-echo "Please choose one of two options\n"
+echo "however it does take more system resources to run than a chroot install"
 
-echo " 1) Virtual machine\n"
+echo " The other option is a chroot install of tollstream's packages."
 
-echo " 2) Chroot\n"
+echo "It is isolated from your parent os as well but does use the same drivers as your parent os"
+
+echo "If you are running linux and not concerned with the greater privacy benefits of using a virtual machine,"
+
+echo " the chroot install option is better on system resources"
+
+echo "Tollstream uses the tiny Alpine linux distribution to be cross compatible with all operating systems"
+
+echo "(tollstream runs in a tiny, isolated os)"
+
+echo -e "Please choose one of two options${ENDCOLOR}"
+
+echo -e "${RED} 1) Virtual machine"
+
+echo -e " 2) Chroot${ENDCOLOR}"
 
 read choice
 
 if [ $choice = 1 ]; then
    virtual_machine
-else 
+else
 
     tollstream_chroot
 fi
