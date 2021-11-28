a6b28b3c07685b72c0ce382c7bddba41261570aa
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2021-11-28 11:36:15 -0500

    Update Alpine_rtmp_tollstream.sh

--- Alpine_rtmp_tollstream.sh
+++ Alpine_rtmp_tollstream.sh
@@ -73,6 +73,7 @@ cp nginx.conf /etc/nginx/nginx.conf
 reset
 chmod +x /home/tollstream-RTMP-server/Tollstream_animated_banner/tollstream_animated.banner
 /home/tollstream-RTMP-server/Tollstream_animated_banner/tollstream_animated.banner
+nginx
 nginx -s reload
 nginx -t
 echo "Your rtmp server is set up. Please answer the questions"
