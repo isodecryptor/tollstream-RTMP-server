724e9849db480fe5eac0eb82e17c8de0153399dd
isodecryptor <72526753+isodecryptor@users.noreply.github.com>
2022-03-31 01:04:37 -0700

    Update test_authentication.sh

--- test_authentication.sh
+++ test_authentication.sh
@@ -7,6 +7,8 @@ nc_listen(){
 
 nc 2001 -w 1 -l  >  userServerInfoCipher.dat
 openssl rsautl -decrypt -in userServerInfoCipher.dat -out netcatfile.txt -inkey /home/ubuntu/private-key->
+#must use code that checks the variables for non command style input. The input can only be treated as a string
+#regardless of what its contents are and then checked for validation or simply thrown out as garbage
 source netcatfile.txt
 serverUploadKey=$(wp user meta get $userName upload_Key --path=/var/www/tollstream.com/)
 if [[ "$serverUploadKey" = "$uploadKey" ]]; then
